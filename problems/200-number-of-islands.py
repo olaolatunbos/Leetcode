0ffb8bb1e7a515d60b1b4ae36c8d2f926047850a
Abdulquadri (Ola) Olatunbosun <olaolat@hotmail.com>
2023-08-14 10:21:12 +0100

    add completed questions

--- problems/200-number-of-islands.py
+++ problems/200-number-of-islands.py
@@ -1,30 +1,34 @@
-class Solution:
-    def numIslands(self, grid) -> int:
+
+class SolutionBFS:
+    def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
 
         rows, cols = len(grid), len(grid[0])
-        visited = set()
-        directions = ((0, 1), (0, -1), (1, 0), (-1, 0))
-        islands = 0
+        visited=set()
+        islands=0
 
-        def traverse(i, j):
-            queue = deque()
-            queue.append((i, j))
+        def bfs(r,c):
+             q = deque()
+             visited.add((r,c))
+             q.append((r,c))
+           
+             while q:
+                 row,col = q.popleft()
+                 directions= [[1,0],[-1,0],[0,1],[0,-1]]
+               
+                 for dr,dc in directions:
+                     r,c = row + dr, col + dc
+                     if (r) in range(rows) and (c) in range(cols) and grid[r][c] == '1' and (r ,c) not in visited:
+                       
+                         q.append((r , c ))
+                         visited.add((r, c ))
 
-            while queue:
-                curr_i, curr_j = queue.popleft()
-                if ((curr_i, curr_j)) not in visited:
-                    visited.add((curr_i, curr_j))
-                for dx, dy in directions:
-                    next_i, next_j = curr_i + dx, curr_j + dy
-                    if ( 0 <= next_i < rows and 0 <= next_j < cols and grid[next_i][next_j] == "1" and (next_i, next_j) not in visited):
-                        queue.append((next_i, next_j))
+        for r in range(rows):
+             for c in range(cols):
+               
+                 if grid[r][c] == "1" and (r,c) not in visited:
+                     bfs(r,c)
+                     islands +=1 
 
-
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == "1" and (i, j) not in visited:
-                    traverse(i, j)
-                    islands += 1
         return islands
