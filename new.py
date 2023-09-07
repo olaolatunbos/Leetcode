e4db56e855a805d3e13d299d71306bfbba8298ba
Abdulquadri (Ola) Olatunbosun <olaolat@hotmail.com>
2023-09-07 11:57:09 +0100

    any-all

--- new.py
+++ new.py
@@ -1,7 +1,14 @@
-import itertools
+# import collections
+# dit = {"ola": 21}
 
-s = "abc"
-print(list(itertools.repeat(10, 10)))
-
-
+# dit["john"] = 12
+# print(dit["man"])
     
+# d = collections.defaultdict(int)
+
+# d["ola"] = 12
+# print(d["john"])
+
+
+import calendar
+print(calendar.TextCalendar(firstweekday=6).formatyear(2015))
